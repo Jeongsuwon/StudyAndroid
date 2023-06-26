ca4aa7003d7e1ddfedcbc8fa242db76f587246a8
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-06-26 19:31:32 +0900

    안드 프로젝트 메인 페이지(06.26)

--- AndProject/app/src/main/java/com/example/andproject/SplashActivity.java
+++ AndProject/app/src/main/java/com/example/andproject/SplashActivity.java
@@ -1,15 +1,35 @@
 package com.example.andproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.andproject.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
 
-
+    ActivitySplashBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle("FLO");
+        dialog.setCancelable(false);
+        dialog.show();
+
+        new Handler().postDelayed(()->{
+            dialog.dismiss();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1000 * 3);
     }
 }
