78ee09d2c0b419e229a29f243a89da4039078416
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-07-12 23:00:56 +0900

    홈 헤드 캐러셀 / 둘러보기 그리드 작업 필요

---
+++ AndProject/app/src/main/java/com/example/andproject/home/NotifySetActivity.java
@@ -0,0 +1,16 @@
+package com.example.andproject.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.andproject.R;
+
+public class NotifySetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify_set);
+    }
+}
