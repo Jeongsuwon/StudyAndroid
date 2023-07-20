bf49fb5ab3f61da1960d45dd27182e4d4a905ad0
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-07-20 13:07:42 +0900

    안드로이드 갤러리

--- Project02_LastProject/app/src/main/java/com/example/project02_lastproject/member/LoginActivity.java
+++ Project02_LastProject/app/src/main/java/com/example/project02_lastproject/member/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.project02_lastproject.member;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,11 +9,14 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.Uri;
 import android.os.Binder;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.Toast;
@@ -47,12 +51,13 @@ public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
 
+    SharedPreferences pref; //자바 변수나 static등의 멤버로 데이터를 저장하면 앱을 다시 실행시 처음부터 불러옴.(DB)
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        checkPermission();
 
         KakaoSdk.init(this, "69d30d825ba2371d3cfbc070e6f9eafd");
         NaverIdLoginSDK.INSTANCE.initialize(this, "hBnKDy9PBaiaYdrOx8fn", "7GZ3buAyjX", "Project02_LastProject");
@@ -80,8 +85,21 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
+        pref = getPreferences(MODE_PRIVATE);
+        editor = pref.edit(); //데이터를 넣거나 수정하기 위한 객체
+        editor.putInt("permission", 0); //데이터 0이 들어감.
+//        editor.apply(); //데이터를 확실히 넣음
 
+        pref.getInt("permission", -1);
+         //최초 실행 시 숫자, String (-1)
+         //한 번 실행 후 -> ?
+         //두 번 실행 후 -> ?
+         //거절 ->
+         //승인 ->
 
+        if(pref.getInt("permission", -1) == -1){
+            checkPermission();
+        }
     }
 
     public void login(){
@@ -223,33 +241,77 @@ public class LoginActivity extends AppCompatActivity {
 
     //나중에 재사용이 가능하게 commonMethod등의 클래스 내부에 넣어두면 좋음.
     private final int REQ_PERMISSION = 1000;
+    private final int REQ_PERMISSION_DENY = 1001;
     private void checkPermission(){
-        String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.ACCESS_MEDIA_LOCATION
-        }; // 카메라 권한을 String으로 가져옴.
-        for (int i = 0; i < permissions.length; i++) {
-            if(ActivityCompat.checkSelfPermission(this, permissions[i]) == PackageManager.PERMISSION_DENIED ){
-                if(ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])){
+        ////        editor.putInt("permission" , 0);//데이터 0이 들어감.
+        ////        editor.apply();//데이터를 확실히 넣음.
+        int permission = pref.getInt("permission" , -1);
+        permission++;
+        editor.putInt("permission" , permission);
+        editor.apply();
+
+        String[] permissions = { Manifest.permission.CAMERA  ,
+                Manifest.permission.ACCESS_MEDIA_LOCATION,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+        } ;//카메라 권한을 String으로 가져옴.
+        // ContextCompat(액티비티가 아닌곳) , ActivityCompat(액티비티)
+        for(int i = 0 ; i <permissions.length ; i ++){
+            //내가 모든 권한이 필요하다면 전체 권한을 하나씩 체크해서 허용 안됨이 있는경우 다시 요청을 하게 만든다.
+            if(ActivityCompat.checkSelfPermission(this , permissions[i]) == PackageManager.PERMISSION_DENIED) {
+                if(ActivityCompat.shouldShowRequestPermissionRationale(this , permissions[i])){
+                    //최초 앱이 설치되고 실행 시 false가 나옴.=>사용자가 거부 후 true 재거부=>false
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("권한 요청").setMessage("권한이 반드시 필요합니다.!!미허용시 앱 사용 불가!");
+                    builder.setPositiveButton("확인(권한허용)" , (dialog, which) -> {
+                        //2.권한 설명 후 다시보여줌.
+                        ActivityCompat.requestPermissions(this, permissions, REQ_PERMISSION_DENY);
+                    });
+                    builder.setNegativeButton("종료(권한허용불가)" , (dialog, which) -> {
+                        finish();
+                    });
+                    builder.create().show();//<==넣어줘야함.
+                }else{
+                    //1.
                     ActivityCompat.requestPermissions(this, permissions, REQ_PERMISSION);
                 }
                 break;
             }
         }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(REQ_PERMISSION == requestCode){
+            for (int i = 0; i < grantResults.length; i++) {
+                if(grantResults[i] == PackageManager.PERMISSION_DENIED){
+                    //거절된권한이있음.
+                    checkPermission();
+                    break;
+                }
+            }
 
-        //ContextCompat(액티비티가 아닌곳), ActivityCompat(액티비티)
-        //int result = ActivityCompat.checkSelfPermission(this, permissions[0]);
-        //Log.d("권한", "checkPermission: " + result);
-//        Log.d("권한", "checkPermission: " + PackageManager.PERMISSION_GRANTED);
-//        Log.d("권한", "checkPermission: " + PackageManager.PERMISSION_DENIED);
+            Log.d("권한", "onRequestPermissionsResult: 권한 요청 완료 ");
+        }else if(REQ_PERMISSION_DENY == requestCode){
+            for (int i = 0; i < grantResults.length; i++) {
+                if(grantResults[i] == PackageManager.PERMISSION_DENIED){
+                    Log.d("권한", "onRequestPermissionsResult: 권한 요청 완료 ");
+                    editor.putInt("permission", -2);
+                    // 3.
+                    viewSetting();
+//                    checkPermission();
+                }
+            }
 
-//        if(ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])){
-//            Log.d("권한", "shouldShowRequestPermissionRationale: 설명이 필요한 권한 ");
-//            ActivityCompat.requestPermissions(this, permissions, REQ_PERMISSION);
-//        }else{
-//            Log.d("권한", "shouldShowRequestPermissionRationale: 설명이 필요x ");
-//            ActivityCompat.requestPermissions(this, permissions, REQ_PERMISSION);
-//        }
+
+        }
     }
 
-
+    public void viewSetting(){
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_SETTINGS);
+        intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+        startActivity(intent);
+    }
 
 }
